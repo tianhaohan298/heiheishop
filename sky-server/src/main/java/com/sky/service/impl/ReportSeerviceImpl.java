bdf0eea900712be3ca5c2a63c7527baf67dd253a
tianheihei298 <tianheihei298@163.com>
2024-09-10 23:47:29 +0800

    营业额数据统计代码开发

---
+++ sky-server/src/main/java/com/sky/service/impl/ReportSeerviceImpl.java
@@ -0,0 +1,67 @@
+package com.sky.service.impl;
+
+import com.github.pagehelper.util.StringUtil;
+import com.sky.entity.Orders;
+import com.sky.mapper.OrderMapper;
+import com.sky.service.ReportService;
+import com.sky.vo.TurnoverReportVO;
+import lombok.extern.slf4j.Slf4j;
+import org.apache.commons.lang.StringUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.LocalTime;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+@Service
+@Slf4j
+public class ReportSeerviceImpl implements ReportService {
+
+    @Autowired
+    private OrderMapper orderMapper;
+
+    /**
+     * 统计指定时间区间的营业额数据
+     * @param begin
+     * @param end
+     * @return
+     */
+    public TurnoverReportVO getTurnoverStatistics(LocalDate begin, LocalDate end) {
+
+        List<LocalDate> dateList = new ArrayList<>();
+        dateList.add(begin);
+
+        while (!begin.equals(end)){
+            begin = begin.plusDays(1);
+            dateList.add(begin);
+        }
+
+        //存放每天的营业额
+        List<Double> turnoverList = new ArrayList<>();
+        for(LocalDate date : dateList){
+            LocalDateTime beginTime = LocalDateTime.of(date, LocalTime.MIN);
+            LocalDateTime endTime = LocalDateTime.of(date, LocalTime.MAX);
+
+            Map map = new HashMap();
+            map.put("begin",beginTime);
+            map.put("end",endTime);
+            map.put("status", Orders.COMPLETED);
+            Double turnover = orderMapper.sumByMap(map);
+
+            turnover = turnover == null ? 0.0 : turnover;
+
+            turnoverList.add(turnover);
+
+        }
+
+        return TurnoverReportVO.builder()
+                .dateList(StringUtils.join(dateList,","))
+                .turnoverList(StringUtils.join(turnoverList,","))
+                .build();
+    }
+}
